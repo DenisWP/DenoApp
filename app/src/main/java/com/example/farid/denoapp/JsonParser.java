0b2ff07cc5a70bbdcf45e7ae97efb0721a5f0960
DenisWP <denis.dabliu@gmail.com>
2017-08-05 22:16:53 -0300

    Melhorias [JsonParser e AsyncTask]

---
+++ app/src/main/java/com/example/farid/denoapp/JsonParser.java
@@ -0,0 +1,46 @@
+package com.example.farid.denoapp;
+
+/**
+ * Created by Denis Wilson on 05/08/2017.
+ */
+
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+
+public class JsonParser {
+
+
+    //readJsonFromUrl = LerJsonUrl
+
+    public static JSONObject LerJsonUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try  {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = Ler(rd);
+            return new JSONObject(jsonText);
+        } finally {
+            is.close();
+        }
+    }
+
+    //readAll = Ler
+
+    private static String Ler(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+}
--- app/src/main/java/com/example/farid/denoapp/TelaInicial.java
+++ app/src/main/java/com/example/farid/denoapp/TelaInicial.java
@@ -1,30 +1,32 @@
 package com.example.farid.denoapp;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.*;
-
-import org.json.JSONArray;
+import android.os.AsyncTask;
+import android.view.View;
+import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.IOException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+
 
 
 public class TelaInicial extends AppCompatActivity {
 
 
     /* declarando os objetos criados. */
-    private ImageView LogoFarid;
-    private EditText edtCodBarras;
-    private TextView txtCodBarras, txtDescProduto, txtSifrao, txtValor;
+    ImageView LogoFarid;
+    EditText edtCodBarras;
+    TextView txtCodBarras, txtCodProduto, txtDescProduto, txtSifrao, txtValor;
 
-    private ProgressDialog pDialog;
 
-    ArrayList<HashMap<String, String>> produtos;
+    String[] objetos = new String[3];
+    String regiao = "1";
+
+
+    JSONObject jsonObjectTexts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,22 +34,62 @@ public class TelaInicial extends AppCompatActivity {
         setContentView(R.layout.activity_tela_inicial);
 
         LogoFarid = (ImageView) findViewById(R.id.LogoFarid);
-        edtCodBarras = (EditText) findViewById(R.id.edtCodBarras);
         txtCodBarras = (TextView) findViewById(R.id.txtCodBarras);
         txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
         txtSifrao = (TextView) findViewById(R.id.txtSifrao);
         txtValor = (TextView) findViewById(R.id.txtValor);
-
-        String site = "http://192.168.0.12:8001/api/Produtos?regiao=1&codigobarra="+ edtCodBarras;
-
-        produtos = new ArrayList<>();
+        edtCodBarras = (EditText) findViewById(R.id.edtCodBarras);
 
 
+
+        String pegaCodBarras = edtCodBarras.getText().toString();
+        String url = "http://192.168.0.12:8001/api/Produtos?regiao=" + regiao + "1&codigobarra="+ pegaCodBarras;
+
+
+
+
+
+        new AsyncTaskExample().execute(url);
     }
 
 
 
+    public class AsyncTaskExample extends AsyncTask<String, String, String[]> {
 
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String[] doInBackground(String... url) {
+
+            try {
+                jsonObjectTexts = JsonParser.LerJsonUrl(url[0]);
+                objetos[0] = jsonObjectTexts.getString("codigo");
+                objetos[1] = jsonObjectTexts.getString("descricao");
+                objetos[2] = jsonObjectTexts.getString("valor");
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+
+            return objetos;
+        }
+
+        @Override
+        protected void onPostExecute(String[] stringFromDoInBackground) {
+
+            txtCodProduto = (TextView) findViewById(R.id.txtCodProduto);
+            txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
+            txtValor = (TextView) findViewById(R.id.txtValor);
+
+
+
+            txtCodBarras.setText(stringFromDoInBackground[0]);
+            txtDescProduto.setText(stringFromDoInBackground[1]);
+            txtValor.setText(stringFromDoInBackground[2]);
+
+        }
+    }
 
 
 }
