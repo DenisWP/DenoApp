657134f85f3b5521b4aa3cb407daa547881b46e1
DenisWP <denis.dabliu@gmail.com>
2017-08-07 09:52:12 -0300

    [Pegando os Dados [SEM Descrição na URL]]

--- app/src/main/java/com/example/farid/denoapp/TelaInicial.java
+++ app/src/main/java/com/example/farid/denoapp/TelaInicial.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.*;
 import android.os.AsyncTask;
 import org.json.JSONException;
@@ -21,6 +22,9 @@ public class TelaInicial extends AppCompatActivity {
     TextView txtCodBarras, txtCodProduto, txtDescProduto, txtSifrao, txtValor;
     String[] objetos = new String[3];
     String regiao = "1";
+    String pegaCodBarras;
+    Button btnVer, btnLimpar;
+    String url = "http://192.168.0.12:8001/api/Produtos?regiao=1&codigobarra=123456";
 
 
     JSONObject jsonObjectTexts;
@@ -31,20 +35,41 @@ public class TelaInicial extends AppCompatActivity {
         setContentView(R.layout.activity_tela_inicial);
 
         LogoFarid = (ImageView) findViewById(R.id.LogoFarid);
-        txtCodBarras = (TextView) findViewById(R.id.txtCodBarras);
-        txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
+        //txtCodBarras = (TextView) findViewById(R.id.txtCodBarras);
+        //txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
         txtSifrao = (TextView) findViewById(R.id.txtSifrao);
-        txtValor = (TextView) findViewById(R.id.txtValor);
+       //txtValor = (TextView) findViewById(R.id.txtValor);
         edtCodBarras = (EditText) findViewById(R.id.edtCodBarras);
 
+        /*Botões para testes*/
+
+        pegaCodBarras = edtCodBarras.getText().toString();
+        //final String url = "http://192.168.0.12:8001/api/Produtos?regiao=" + regiao + "1&codigobarra="+ pegaCodBarras;
 
 
-        String pegaCodBarras = edtCodBarras.getText().toString();
-        final String url = "http://192.168.0.12:8001/api/Produtos?regiao=" + regiao + "1&codigobarra="+ pegaCodBarras;
+        btnVer = (Button) findViewById(R.id.btnVer);
+        btnVer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AsyncTaskExample().execute(url);
+            }
+        });
 
 
 
-        edtCodBarras.addTextChangedListener(new TextWatcher() {
+        btnLimpar = (Button) findViewById(R.id.btnLimpar);
+        btnLimpar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limparDados();
+            }
+        });
+
+
+
+
+
+        /*edtCodBarras.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -59,11 +84,16 @@ public class TelaInicial extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
                 new AsyncTaskExample().execute(url);
             }
-        });
+        });*/
 
 
     }
 
+    public void limparDados(){
+        txtCodProduto.setText("");
+        txtDescProduto.setText("");
+        txtValor.setText("");
+    }
 
 
     public class AsyncTaskExample extends AsyncTask<String, String, String[]> {
@@ -77,9 +107,9 @@ public class TelaInicial extends AppCompatActivity {
 
             try {
                 jsonObjectTexts = JsonParser.LerJsonUrl(url[0]);
-                objetos[0] = jsonObjectTexts.getString("codigo");
-                objetos[1] = jsonObjectTexts.getString("descricao");
-                objetos[2] = jsonObjectTexts.getString("valor");
+                objetos[0] = jsonObjectTexts.getString("Codigo");
+                objetos[1] = jsonObjectTexts.getString("Descricao");
+                objetos[2] = jsonObjectTexts.getString("Preco");
             } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
@@ -90,13 +120,13 @@ public class TelaInicial extends AppCompatActivity {
         @Override
         protected void onPostExecute(String[] stringFromDoInBackground) {
 
-            //txtCodProduto = (TextView) findViewById(R.id.txtCodProduto);
-            //txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
-            //txtValor = (TextView) findViewById(R.id.txtValor);
+            txtCodProduto = (TextView) findViewById(R.id.txtCodProduto);
+            txtDescProduto = (TextView) findViewById(R.id.txtDescProduto);
+            txtValor = (TextView) findViewById(R.id.txtValor);
 
 
 
-            txtCodBarras.setText(stringFromDoInBackground[0]);
+            txtCodProduto.setText(stringFromDoInBackground[0]);
             txtDescProduto.setText(stringFromDoInBackground[1]);
             txtValor.setText(stringFromDoInBackground[2]);
 
