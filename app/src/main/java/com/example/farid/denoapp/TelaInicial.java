7ccef599532df1d0ec7e4b1f0f8066d2bd6945d7
DenisWP <denis.dabliu@gmail.com>
2017-08-08 17:00:27 -0300

    Adequações

--- app/src/main/java/com/example/farid/denoapp/TelaInicial.java
+++ app/src/main/java/com/example/farid/denoapp/TelaInicial.java
@@ -1,11 +1,7 @@
 package com.example.farid.denoapp;
 
-
-import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.*;
@@ -21,10 +17,10 @@ public class TelaInicial extends AppCompatActivity implements View.OnKeyListener
 
     ImageView LogoFarid;
     EditText edtCodBarras;
-    TextView txtCodBarras, txtCodProduto, txtDescProduto, txtSifrao, txtValor;
+    TextView txtCodProduto, txtDescProduto, txtSifrao, txtValor;
     String[] objetos = new String[3];
     String regiao = "1";
-    Button btnVer, btnLimpar;
+    Button btnVer;
     String url, CB;
     JSONObject jsonObjectTexts;
 
@@ -46,35 +42,35 @@ public class TelaInicial extends AppCompatActivity implements View.OnKeyListener
         carregaEnter();
     }
 
-
-
     /*
     * Função criada para testar se a tecla "ENTER" do teclado ou o "Conclui." foi pressionada.
     * */
     public void carregaEnter (){
         btnVer.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 CB = edtCodBarras.getText().toString();
-                url = "http://192.168.0.12:8001/api/Produtos?regiao=" +regiao+ "&codigobarra="+CB;
-                new AsyncTaskExample().execute(url);
+                    url = "http://192.168.0.12:8001/api/Produtos?regiao=" +regiao+ "&codigobarra="+CB;
+                    new AsyncTaskExample().execute(url);
+                 /*Thred criada para apagar os campos após 5 segundos*/
 
-                /*Thred criada para apagar os campos após 5 segundos*/
-                new Thread(new Runnable() {
+
+                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            Thread.sleep(7000);
+                            Thread.sleep(5000);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                limparDados();
-                            }
-                        });
+                            runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        limparDados();
                     }
+                });
+            }
                 }).start();
             }
         });
